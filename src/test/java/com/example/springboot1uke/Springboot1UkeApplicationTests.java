229195dde99a2031d3af9c8e7477251ea81e93d5
hannaosollie <hannaosollie@gmail.com>
2023-02-23 12:17:02 +0100

    Initial commit

---
+++ src/test/java/com/example/springboot1uke/Springboot1UkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot1uke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1UkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
